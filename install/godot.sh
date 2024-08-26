3bcc01bce8068d65f39ebef024622a7f65976968
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2024-08-26 19:38:33 -0300

    Update install script for Godot.

--- install/godot.sh
+++ install/godot.sh
@@ -1,7 +1,6 @@
 cd /tmp
-wget -O godot.zip 'https://github.com/godotengine/godot/releases/download/4.2.2-stable/Godot_v4.2.2-stable_linux.x86_64.zip'
+wget -O godot.zip 'https://github.com/godotengine/godot/releases/download/4.3-stable/Godot_v4.3-stable_linux.x86_64.zip'
 unzip godot.zip -d godot
 sudo mv godot/Godot* /usr/local/bin/godot
-rm godot.zip
-rm -rf godot
+rm -rf godot.zip godot
 cd -
