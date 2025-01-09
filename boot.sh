056b4e83e5c5f11e6541ac5fda1e40c6bf124295
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2025-01-09 02:05:14 -0300

    Improve boot.sh.

--- boot.sh
+++ boot.sh
@@ -1,3 +1,6 @@
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 echo "Installing Git..."
 sudo add-apt-repository -y ppa:git-core/ppa > /dev/null
 sudo apt update > /dev/null
@@ -5,6 +8,7 @@ sudo apt install -y git > /dev/null
 
 echo "Cloning my environment..."
 mkdir -p ~/Dropbox/Projetos/Ambiente
+rm -rf ~/Dropbox/Projetos/Ambiente/my-ubuntu-environment
 git clone https://github.com/elomarns/my-ubuntu-environment.git ~/Dropbox/Projetos/Ambiente/my-ubuntu-environment > /dev/null
 
 echo "Installing my environment..."
