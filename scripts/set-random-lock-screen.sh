16c076b9a2df4299fa37a063958364441681daf0
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2025-01-10 01:15:52 -0300

    Improve the comment in the script that sets a random lock screen.

--- scripts/set-random-lock-screen.sh
+++ scripts/set-random-lock-screen.sh
@@ -3,7 +3,8 @@
 # Directory containing the wallpapers.
 WALLPAPER_DIR=$1
 
-# Choose a random wallpaper from the directory.
+# Choose a random wallpaper from the directory. As i3lock doesn't seem to work
+# with jpg/jpeg images, only png images are considered.
 SELECTED_WALLPAPER=$(find "$WALLPAPER_DIR" -type f -name "*.png" | shuf -n 1)
 
 # Set the chosen wallpaper as the background for i3lock.
