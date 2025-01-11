1d40d85246dcda43350d6531f93addf949ea1ef1
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2025-01-11 01:15:34 -0300

    Improve the install script for Docker.

--- installers/docker.sh
+++ installers/docker.sh
@@ -16,6 +16,8 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Setting the user to run Docker commands without sudo.
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+
+# Limit log size to avoid running out of disk.
+echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
