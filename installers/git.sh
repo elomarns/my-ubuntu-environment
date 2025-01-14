8771f91de453d0e01da3f9df1a2d10d868dffcb6
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2025-01-14 03:05:59 -0300

    Fix the install script for Git. Now the name is set properly.

--- installers/git.sh
+++ installers/git.sh
@@ -2,7 +2,7 @@
 sudo apt install -y git
 
 # Set Git.
-git config --global user.name $NAME
+git config --global user.name "$NAME"
 git config --global user.email $EMAIL
 git config --global init.defaultBranch main
 git config --global core.editor "code --wait"
