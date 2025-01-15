b493c600170f09571a1f99b8eb2e717917f58857
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2025-01-15 02:24:04 -0300

    Set a password forr the SSH key.

--- installers/ssh-key.sh
+++ installers/ssh-key.sh
@@ -1,5 +1,5 @@
 # Create a new SSH key.
-ssh-keygen -t ed25519 -C $EMAIL
+ssh-keygen -t ed25519 -C $EMAIL -f ~/.ssh/id_ed25519 -N "$PASSWORD"
 
 # Start the ssh-agent in the background.
 eval "$(ssh-agent -s)"
