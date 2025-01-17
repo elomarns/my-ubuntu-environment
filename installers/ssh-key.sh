87998ed1846ead6e3e4d8331400e6e8e9755e592
Elomar Nascimento dos Santos <elomarns@elomarns.com>
2025-01-17 03:19:39 -0300

    Improve that sets my SSH key.

--- installers/ssh-key.sh
+++ installers/ssh-key.sh
@@ -1,5 +1,5 @@
 # Create a new SSH key.
-ssh-keygen -t ed25519 -C $EMAIL -f ~/.ssh/id_ed25519 -N ""
+ssh-keygen -t ed25519 -C $EMAIL -f ~/.ssh/id_ed25519
 
 # Start the ssh-agent in the background.
 eval "$(ssh-agent -s)"
